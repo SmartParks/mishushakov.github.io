9616f760db5df27b15a13de04c3500ec622880d8
MishUshakov <mish.ushakov@gmail.com>
2017-12-12 12:28:37 +0100

    More posts & updated design and more

--- worker.js
+++ worker.js
@@ -15,9 +15,10 @@ self.addEventListener('install', function(event) {
           //'https://imgur.com/2WBewXv.png',
             'https://mish.io/Ushakov/images/icons/TMachine.png',
           //'https://www.ethode.com/contentAsset/image/e65ea977-f4b2-4cc2-8ffc-8a92127b999e/image/filter/Resize/resize_w/400',
-            'https://mish.io/Ushakov/images/icons/mfolder.ico',
+            'https://lh3.googleusercontent.com/Gu6CWMrnD_ohDjsDDf8EFxL7yOMygEqzqHT8P_D4GWyRmiasu7f2fal1X5LufYsrzw=w256',
             'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTghWYXueAQVPvV2AVLHbCI3Y6km5n7GBUNZmSsgNeU68UZxifLoA',
-          //'https://qph.ec.quoracdn.net/main-thumb-t-1800-200-lOn8kKfhqfcTcKdt2GwaLfGnC0jEjHmV.jpeg',
+            'https://zdnet4.cbsistatic.com/hub/i/2014/08/07/9010618d-34cf-11e4-9e6a-00505685119a/427b2b85b8a14a7abc2c3ad2024db2cc/zdnet-google-fit.png',
+            'https://console.dialogflow.com/api-client/assets/img/logo-short.png',
           //'https://imgur.com/pjNvoyJ.png',
             'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/VK.com-logo.svg/600px-VK.com-logo.svg.png',
             'https://pbs.twimg.com/profile_images/1250624875/tw-128-btg-v3_400x400.jpg',
@@ -30,6 +31,7 @@ self.addEventListener('install', function(event) {
             'https://pp.userapi.com/c840437/v840437590/15bfd/JKBGlOz6qQ4.jpg',
             'https://pp.userapi.com/c840132/v840132898/3b192/79qTNvmbvz8.jpg',
             'https://vignette1.wikia.nocookie.net/logopedia/images/f/f7/2000px-Google_2015_monochrome.svg.png/revision/latest?cb=20150901175304',
+            'https://png.icons8.com/color/540/raspberry-pi-zero.png'
           //'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Logo.png',
           //'https://pngimg.com/uploads/ibm/ibm_PNG19648.png'
         ])
